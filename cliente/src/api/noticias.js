009b22054ec0effc1e594e449e4d31eab2c85944
Mapib-hub <mapibpublicidad@gmail.com>
2025-05-17 17:39:00 -0400

    Areglando rutas recargables en el servidor de pruebas parte 7

--- cliente/src/api/noticias.js
+++ cliente/src/api/noticias.js
@@ -1,9 +1,8 @@
 import axiosInstance from "./axios"; // Importamos nuestra instancia configurada
 
 export const getNoticiasRequest = () => axiosInstance.get(`/api/noticias`);
-
 // Podrías añadir más funciones aquí en el futuro, como:
- export const getNoticiaRequest = (id) => axiosInstance.get(`/noticias/${id}`);
- export const createNoticiaRequest = (noti) => axiosInstance.post(`/noticias`, noti);
- export const deleteNoticiaRequest = (id) => axiosInstance.delete(`/noticias/${id}`);
- export const updateNoticiaRequest = (id, noti) => axiosInstance.put(`/noticias/${id}`, noti);
+ export const getNoticiaRequest = (id) => axiosInstance.get(`/api/noticias/${id}`);
+ export const createNoticiaRequest = (noti) => axiosInstance.post(`/api/noticias`, noti);
+ export const deleteNoticiaRequest = (id) => axiosInstance.delete(`/api/noticias/${id}`);
+ export const updateNoticiaRequest = (id, noti) => axiosInstance.put(`/api/noticias/${id}`, noti);
