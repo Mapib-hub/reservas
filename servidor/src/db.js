051c49f260874f218b2cc1069e3a735eee3d17a3
Mapib-hub <mapibpublicidad@gmail.com>
2025-05-17 16:20:18 -0400

    Areglando para portainer la coneccion a la base de datoss

--- servidor/src/db.js
+++ servidor/src/db.js
@@ -1,10 +1,15 @@
+// En file:///usr/src/app/src/db.js
 import mongoose from "mongoose";
 
-export const conectDB = async () =>{
+export const conectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost/reservas");
-        console.log(">>> Base de datos conectada");
+        const uriToConnect = process.env.MONGODB_URI; // O como la estés obteniendo
+        console.log("Intentando conectar a MongoDB con URI:", uriToConnect); // <--- AÑADE ESTO
+        await mongoose.connect(uriToConnect);
+        console.log(">>> DB Conectada");
     } catch (error) {
-        console.log(error);
+        console.error("Error al conectar a la DB:", error);
+        // Es importante relanzar el error o manejarlo para que el proceso falle si no puede conectar
+        throw error; 
     }
 };
