8a232591d93a211e1f01a03a6eefe92a10db5713
Mapib-hub <mapibpublicidad@gmail.com>
2025-05-23 12:59:54 -0400

    modificamos el reservas denuevo solo para probar 2!

--- cliente/src/api/reservas.js
+++ cliente/src/api/reservas.js
@@ -9,7 +9,7 @@ export const createReservaRequest = (reservaData) => axios.post(API_URL, reserva
 export const updateReservaRequest = (id, reservaData) => axios.put(`${API_URL}/${id}`, reservaData);
 export const deleteReservaRequest = (id) => axios.delete(`${API_URL}/${id}`);
 export const getDisponibilidadRequest = (canchaId, fecha) => {
-  return axios.get('/disponibilidad/', {
+  return axios.get('/disponibilidad', {
     params: { // Axios construirá la query string: ?canchaId=...&fecha=...
       canchaId: canchaId,
       fecha: fecha
