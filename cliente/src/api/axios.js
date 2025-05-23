8f3f40e6239524a60dc877ea05ab4824abb822de
Mapib-hub <mapibpublicidad@gmail.com>
2025-05-23 11:55:05 -0400

    nuevamente quitamos api y agregamos slash de la ruta!

--- cliente/src/api/axios.js
+++ cliente/src/api/axios.js
@@ -6,4 +6,15 @@ const instance = axios.create({
     withCredentials: true, // Importante si manejas sesiones/cookies con el backend
 });
 
+// Interceptor para depuración (opcional, pero útil para este caso)
+instance.interceptors.request.use(config => {
+    console.log('Enviando solicitud a:', config.url);
+    if (config.params) {
+        console.log('Con parámetros:', config.params);
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+
 export default instance;
